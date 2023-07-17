836bada67dd1156e9e9ce815bfe861ec7755db70
matthew <matthkang@gmail.com>
2023-07-17 16:55:58 -0700

    notification, 24 hour format, readme

--- README.md
+++ README.md
@@ -1,12 +1,16 @@
-# Work Day Scheduler Starter Code
+# Work Day Scheduler
 
 ## Description
 
-- Create a Coding quiz that tests users on their knowledge of coding knowledge and core concepts.
-- Timer implmented where incorrect answers will deduct time. 
-- High Scores feature with leaderboard of users with best time. 
+- Create a scheduler that displays events that happen throughout the day ☀️
+- Events are color coded based on the current hour, depending on if they are in the past, present or future:
+  - ![#d3d3d3](https://placehold.co/15x15/d3d3d3/d3d3d3.png) `events in the past`
+  - ![#ff6961](https://placehold.co/15x15/ff6961/ff6961.png) `events in the present hour`
+  - ![#77dd77](https://placehold.co/15x15/77dd77/77dd77.png) `events in the future`
+- You can save events which will be stored in localStorage 📋
 
-Link to deployed application: 
+## Link to deployed application
+https://matthkang.github.io/workday-scheduler/
 
 
 ## Table of Contents (Optional)
@@ -16,20 +20,14 @@ Link to deployed application:
 
 
 ## Usage
-Below is a screenshot of the initial home page for the coding quiz application:
-![Coding quiz home page](assets/images/quiz-home.png)
-
-Clicking 'Start Quiz' will start the timer and display questions. Choosing the wrong answer will display the correct answer in green and the selected wrong answer in red.
-![Sample Question](assets/images/question.png)
-
-If the timer hits 0 or you have answered all the questions, the quiz is over and you are presented with your final score and an opportunity to save your initials and score to the leaderboard:
-![Game over screen](assets/images/game-over.png)
-
-You can view the high scores at any time by clicking the 'View High Scores' from the home screen:
-![High scores screen](assets/images/high-scores.png)
+Work Day Scheduler with current day and color coded events based on current time
+![Work Day Scheduler](assets/images/scheduler.png)
+![Color-coded Appointments](assets/images/events.png)
 
 
 ## Credits
 
 - https://stackoverflow.com/questions/30237338/how-to-get-id-of-div-on-click-of-button-inside-it
 - https://stackoverflow.com/questions/48239/getting-the-id-of-the-element-that-fired-an-event
+- https://www.freecodecamp.org/news/how-to-convert-a-string-to-a-number-in-javascript/
+- https://www.w3schools.com/jsref/jsref_substring.asp
--- assets/js/script.js
+++ assets/js/script.js
@@ -4,13 +4,9 @@
 $(function () {
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-
-  $(document).ready(function() {
-    $("button").click(function(event) {
+  // local storage.
+  $(document).ready(function () {
+    $("button").click(function (event) {
       var parent_div = $(this).parent();
       var time_value = parent_div.children()[0].textContent;
       var text_area_value = parent_div.children()[1].value;
@@ -24,7 +20,7 @@ $(function () {
       var timesStorage = localStorage.getItem("event");
 
       // store event object in localStorage
-      if (timesStorage !== null){
+      if (timesStorage !== null) {
         var timesArr = JSON.parse(timesStorage);
       }
       else {
@@ -33,41 +29,40 @@ $(function () {
 
       let exists = false;
 
-      for (var i = 0; i < timesArr.length; i++){
+      for (var i = 0; i < timesArr.length; i++) {
         var timeEntry = timesArr[i];
         // if desc already exists for time entry
         if (Object.values(timeEntry).indexOf(time_value) > -1) {
-            // console.log('time exists: ', time_value);
-            // console.log("description was: ", timeEntry.desc);
+          // console.log('time exists: ', time_value);
+          // console.log("description was: ", timeEntry.desc);
 
-            timeEntry.desc = text_area_value;
-            
-            // console.log("description is now: ", timeEntry.desc);
-            exists = true;
+          timeEntry.desc = text_area_value;
+
+          // console.log("description is now: ", timeEntry.desc);
+          exists = true;
         }
       }
-      
+
       // first time saving entry, push new object to array
       if (exists === false) {
-          timesArr.push(eventObj);
+        timesArr.push(eventObj);
       }
 
       localStorage.setItem("event", JSON.stringify(timesArr));
+
+      $('#notification').text("Event has been saved to localStorage ✔")
     });
   });
 
   //
   // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
+  // block by comparing the id to the current hour. 
   var scheduleBlock = $('.container-fluid');
   var times = scheduleBlock.children();
 
   var currentHour = dayjs().hour();
 
-  for (var i = 0; i < times.length; i++){
+  for (var i = 0; i < times.length; i++) {
     var entry = times[i];
     var hourId = entry.id; // "hour-9"
     var entryJs = $('#' + hourId)
@@ -75,11 +70,11 @@ $(function () {
     var hour = hourId.split('-')[1];
 
     // in the past
-    if (hour < currentHour){
+    if (hour < currentHour) {
       entryJs.removeClass().addClass("row time-block past");
     }
     // in the present
-    else if (hour == currentHour){
+    else if (hour == currentHour) {
       entryJs.removeClass().addClass("row time-block present");
     }
     // in the future
@@ -90,27 +85,36 @@ $(function () {
 
   //
   // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
+  // the values of the corresponding textarea elements.
   var storage = localStorage.getItem("event");
   var storageArr = JSON.parse(storage);
-  console.log("storage: ", storageArr);
-  if (storageArr !== null){
-    for (var i = 0; i < storageArr.length; i++){
+  // console.log("storage: ", storageArr);
+  if (storageArr !== null) {
+    for (var i = 0; i < storageArr.length; i++) {
       var entry = storageArr[i];
       var time = entry.time;
-      var hour = time.substring(0, time.length-2);
-  
+
+      // convert to 24 hour format
+      if ((time.substring(time.length - 2) === 'PM') && time.substring(0, time.length - 2) != 12) {
+        var hour = time.substring(0, time.length - 2);
+        hour = Number(hour) + 12;
+      }
+      else {
+        var hour = time.substring(0, time.length - 2);
+      }
+
+      // console.log(hour);
+
       var desc = entry.desc;
-  
+
       // target block with corresponding hour id 
       var timeEl = $('#hour-' + hour);
-  
+
       // set desc for entry from localStorage
       timeEl.children()[1].value = desc;
     }
   }
-  
+
   //
   // TODO: Add code to display the current date in the header of the page.
   var today = dayjs().format('dddd, MMMM D')
